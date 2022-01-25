46d1a0bbd5e3178a51de2ed3922ffc1b2a97e1e3
Amrata-Shet <56686812+Amrata-Shet@users.noreply.github.com>
2022-01-26 00:00:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function country_graph(){
     let code=document.querySelector(".country_code").value;
     let url=`https://corona.dnsforfamily.com/graph.png?c=${code}`;
-    var anchors = document.querySelector('a[href*="https://corona.dnsforfamily.com/graph.png?c=IN"]');	
+    var anchors = document.querySelectorAll('a[href*="https://corona.dnsforfamily.com/graph.png?c=IN"]');	
 Array.prototype.forEach.call(anchors, function (element, index) {	   
  element.href = url;
                 });
